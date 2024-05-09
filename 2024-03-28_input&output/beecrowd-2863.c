2f90e509d762ba5ff5095ecd57c44881e5a66f51
marcos-0215 <149538998+Marcos-0215@users.noreply.github.com>
2024-05-09 20:12:13 -0300

    Beecrowd exerc 2863 complete

---
+++ 2024-03-28_input&output/beecrowd-2863.c
@@ -0,0 +1,27 @@
+// Completed 11 APRIL 2024
+
+// LINK: https://judge.beecrowd.com/pt/problems/view/2863
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    while (scanf("%d", &n) != EOF) {
+        float run, min = 99.9;
+        
+        for (int i=0; i<n; i++) {
+            scanf("%f", &run);
+            
+            if (run < min) {
+                min = run;
+            }
+        }
+        
+        printf("%.2f\n", min);
+    }
+    
+    
+    return 0;
+}
